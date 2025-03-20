3dd3a022c85de21a1ae0931c81d41f02713779d8
Dipak Khade <dipakhade214@gmail.com>
2025-03-20 22:58:29 +0530

    added golbal vars

--- apps/hub/index.ts
+++ apps/hub/index.ts
@@ -1,1 +1,14 @@
-console.log("Hello via Bun!");
+import { randomUUIDv7, type ServerWebSocket } from "bun";
+import { type HubIncomingMessage, type HubOutgoingMessage } from "@uptime/types";
+
+const AVAILABLE_VALIDADATORS : {
+    publicKey: string,
+    validatorId: string,
+    socket: ServerWebSocket<unknown>
+} = {}
+
+const CALLBACKS : {
+    [callbackid:string]:(data:HubIncomingMessage)=>void
+} = {}
+
+const VALIDATOR_INSENTIVE_LAMPORTS = 100;
