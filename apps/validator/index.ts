a562481d6ab1f87da5c121e641a7e30b062aca26
Dipak Khade <dipakhade214@gmail.com>
2025-03-24 21:39:56 +0530

    validator logic checkpoint

--- apps/validator/index.ts
+++ apps/validator/index.ts
@@ -31,7 +31,7 @@ function main(){
         }))
     }
 
-    ws.onmessage = async (event) => {
+    ws.onmessage = (event) => {
        const data = event.data as HubOutgoingMessage;
 
        switch(data.type){
@@ -42,7 +42,7 @@ function main(){
             break;
         case "validate":
             const {callbackId:callback_id, url, websiteId} = data.data;
-            await validateMessageHandler(callback_id, url, websiteId);
+            validateMessageHandler(callback_id, url, websiteId, ws);
             break;
        }
     }
@@ -56,8 +56,34 @@ function signMessage(privateKey:Uint8Array<ArrayBufferLike>, message:string){
     return JSON.stringify(Array.from(signature));
 }
 
-function validateMessageHandler(callback_id:string, url:string, websiteId:string){
+function validateMessageHandler(callback_id:string, url:string, websiteId:string, ws:WebSocket){
     console.log(`got validate message ${url}`);
+    const signedMessage = signMessage(Uint8Array.from(process.env.SOALANA_PRIVATE_KEY!), `this is a message from validator ${validatorId} for website ${websiteId}`);
+    const startTime = Date.now();
+    fetch(url).then(async (res) => {
+        const endTime = Date.now();
+        const latency = endTime - startTime;
+        const status = res.status;
+        console.log(`got response from ${url} with status ${status} and latency ${latency}`);
+        ws.send(JSON.stringify({
+            callbackId:callback_id,
+            websiteId,
+            validatorId,
+            latency,
+            signedMessage,
+            status : status === 200 ? "good" : "bad"
+        }))
+    }).catch((err:any)=>{
+        const endTime = Date.now();
+        ws.send(JSON.stringify({
+            callbackId:callback_id,
+            websiteId,
+            validatorId,
+            latency:endTime-startTime,
+            signedMessage,
+            status: "bad"
+        }))
+    })
 }
 
 main();
